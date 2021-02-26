095762487a7f04bbbd88be1b92cc17ea2e0e6c2b
david042 <david.mal04@hotmail.com>
2021-02-26 09:10:28 +0900

    acção de soma

--- www/components/accao.js
+++ www/components/accao.js
@@ -1,2 +1,10 @@
 // This is a JavaScript file
 
+$(document).on("click","#somar",function(){
+  var valor1 = $("#valor1").val();
+  var valor1 = $("#valor1").val();
+
+  var resultado = parseFloat(valor1) + parseFloat(valor2);
+
+  $("#resultado").val(resultado);
+});
