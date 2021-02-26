b2f544cb8ef7f9b4f877b69116da06db7ca6c012
david042 <david.mal04@hotmail.com>
2021-02-26 10:31:17 +0900

    botões de subtração, divisão e multiplicação

--- www/components/accao.js
+++ www/components/accao.js
@@ -8,3 +8,30 @@ $(document).on("click","#somar",function(){
 
   $("#resultado").val(resultado);
 });
+
+$(document).on("click","#subtrair",function(){
+  var valor1 = parseFloat($("#valor1").val());
+  var valor2 = parseFloat($("#valor2").val());
+
+  var resultado = valor1 - valor2;
+
+  $("#resultado").val(resultado);
+});
+
+$(document).on("click","#dividir",function(){
+  var valor1 = parseFloat($("#valor1").val());
+  var valor2 = parseFloat($("#valor2").val());
+
+  var resultado = valor1 / valor2;
+
+  $("#resultado").val(resultado);
+});
+
+$(document).on("click","#multiplicar",function(){
+  var valor1 = parseFloat($("#valor1").val());
+  var valor2 = parseFloat($("#valor2").val());
+
+  var resultado = valor1 * valor2;
+
+  $("#resultado").val(resultado);
+});
